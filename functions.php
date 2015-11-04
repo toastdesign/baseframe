40d08dd0e1a8c07f1113151b6d3428f8fb0b0df5
Toast Design <info@toastdesign.nl>
2015-11-04 14:01:14 +0100

    better enque bootstrap js

--- functions.php
+++ functions.php
@@ -120,7 +120,7 @@ function toasttheme_scripts() {
 
 	wp_enqueue_script( 'toasttheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	wp_enqueue_script( 'toasttheme-bootstrap-scripts', get_template_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js', array(), '20130115', true );
+	wp_enqueue_script( 'toasttheme-bootstrap-scripts', get_template_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js', array('jquery'), '20151104', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -157,3 +157,11 @@ require get_template_directory() . '/inc/jetpack.php';
  * Load BFI Thumb
  */
 require get_template_directory() . '/inc/BFI_Thumb.php';
+
+
+/**
+ * Load bootstrap walker
+ */
+require get_template_directory() . '/inc/wp_bootstrap_navwalker.php';
+
+
