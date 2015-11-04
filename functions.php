cb19e3cd5b4254660c1e4ffcf8248cd9194ac077
Toast Design <info@toastdesign.nl>
2015-11-04 13:40:18 +0100

    Added bootstrap scripts to functions.php

--- functions.php
+++ functions.php
@@ -120,6 +120,8 @@ function toasttheme_scripts() {
 
 	wp_enqueue_script( 'toasttheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script( 'toasttheme-bootstrap-scripts', get_template_directory_uri() . '/bower_components/bootstrap/dist/js/bootstrap.min.js', array(), '20130115', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
